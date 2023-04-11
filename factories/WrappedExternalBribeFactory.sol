ea08ad360b455231975833a7d30d3cea97c6f676
Bill <bill@auragi.finance>
2023-04-11 14:43:51 +0700

    first commit

---
+++ factories/WrappedExternalBribeFactory.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import {WrappedExternalBribe} from 'contracts/WrappedExternalBribe.sol';
+
+contract WrappedExternalBribeFactory {
+    address public immutable voter;
+    mapping(address => address) public oldBribeToNew;
+    address public last_bribe;
+
+    event CreateWrapExternalBribe(address old_bribe, address wrap_ex_bribe);
+
+    constructor(address _voter) {
+        voter = _voter;
+    }
+
+    function createBribe(address existing_bribe) external returns (address) {
+        require(
+            oldBribeToNew[existing_bribe] == address(0),
+            "Wrapped bribe already created"
+        );
+        last_bribe = address(new WrappedExternalBribe(voter, existing_bribe));
+        oldBribeToNew[existing_bribe] = last_bribe;
+
+        emit CreateWrapExternalBribe(existing_bribe, last_bribe);
+        return last_bribe;
+    }
+}
